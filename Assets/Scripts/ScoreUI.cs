9cf591e2d116e8a363202ea3fde4cd5d41eb7e7b
souliflame <souliflame01@gmail.com>
2021-02-19 21:24:53 +0900

    1: Add score UI 2: delete Mode(To be added later) 3: Build 210219

---
+++ Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreUI : MonoBehaviour
+{
+    public GameObject[] scoreTexts = new GameObject[10];
+    public string mainSceneName;
+    private HandleScorefile handler;
+
+    private void Awake()
+    {
+        handler = GetComponent<HandleScorefile>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] scores = handler.LoadScoreData();
+        for (int i = 0; i < scoreTexts.Length; ++i)
+        {
+            scoreTexts[i].GetComponent<Text>().text = $"#{i + 1} - {scores[i].ToString()}";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            SceneManager.LoadScene(mainSceneName);
+
+        }
+    }
+}
